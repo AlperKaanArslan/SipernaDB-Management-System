e7f3abfda55dc6456f40026f1056ad4a30652aea
Alper Kaan ARSLAN <alperkaan0705@gmail.com>
2026-04-06 18:40:31 +0200

    Add files via upload

---
+++ web/DBConnection.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient; 
+
+namespace SipernaWeb
+{
+    public class DBConnection
+    {
+        private readonly string _connectionString = "Server=DESKTOP-VEBT1KK;Database=SipernaDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public DataSet getSelect(string sqlstr)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(sqlstr, con);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+        }
+
+        public bool execute(string sqlstr)
+        {
+            using (SqlConnection con = new SqlConnection(_connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(sqlstr, con);
+                con.Open();
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+    }
+}
